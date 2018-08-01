aef91fb87394c375175c60495bc375b2b49e20c1
David Jewell <david@david-jewell.net>
2018-08-01 17:56:28 -0400

    Adds fun bg effects on mouse move

---
+++ _src/js/splash.js
@@ -0,0 +1,44 @@
+// splash screen js
+theWindow = $(window);
+theBG = $('#bg');
+
+var pageInfo = {
+  pageWidth: theWindow.width(),
+  pageHeight: theWindow.height(),
+  mouseXPos: 0,
+  mouseYPos: 0
+}
+
+// create mouse event listener to move bg
+theWindow.mousemove(function (event) {
+  pageInfo.mouseXPos = event.pageX;
+  pageInfo.mouseYPos = event.pageY;
+
+  position_background();
+});
+
+$('h1 a').hover(function () {
+  theBG.addClass('hovering');
+},
+  function () {
+    theBG.removeClass('hovering');
+  }
+);
+
+function position_background() {
+
+  var moveX = (pageInfo.pageWidth + pageInfo.mouseXPos) / -10;
+  var moveY = (pageInfo.pageHeight + pageInfo.mouseYPos) / -10;
+
+  theBG.css('background-position-x', moveX).css('background-position-y', moveY);
+}
+
+function rotate_bg() {
+  rotateAmt = Math.random() * 50;
+  theBG.css('transform', 'rotate(' + rotateAmt + 'deg)');
+}
+
+
+window.onload = function () {
+  rotate_bg();
+};
