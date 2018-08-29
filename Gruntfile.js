573ef23ddacabda85266d99b7604693cfb645f1e
David Jewell <david@david-jewell.net>
2018-08-29 17:35:00 -0400

    Now with slideshow action!

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,47 +2,77 @@
 
 // todo: figure out if jshint is needed
 
-module.exports = function (grunt) {
-
+module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    pkg: grunt.file.readJSON("package.json"),
+    banner:
+      "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
       '<%= grunt.template.today("yyyy-mm-dd @ HH:mm") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= pkg.license %> */\n',
+      " Licensed <%= pkg.license %> */\n",
     // Task configuration.
     uglify: {
       options: {
-        banner: '<%= banner %>',
-        sourceMap: true,
+        banner: "<%= banner %>",
+        sourceMap: true
       },
       prod: {
         options: {
           output: {
-            comments: false,
-          },
+            comments: false
+          }
         },
-        files: {
-          'dist/prod/js/jewellnet.min.js': ['_src/js/base.js'],
-          'dist/prod/js/jquery-scrollspy.min.js': ['_src/js/jquery-scrollspy.js'],
-          'dist/prod/splash/js/dj-splash.js': ['_src/js/base.js', '_src/js/splash.js'],
-        }
+        // files: {
+        //   "dist/prod/js/jewellnet.min.js": ["_src/js/base.js"],
+        //   "dist/prod/js/jquery-scrollspy.min.js": [
+        //     "_src/js/jquery-scrollspy.js"
+        //   ],
+        //   "dist/prod/splash/js/dj-splash.js": [
+        //     "_src/js/base.js",
+        //     "_src/js/splash.js"
+        //   ]
+        // }
+        files: [
+          { src: "_src/js/base.js", dest: "dist/prod/js/jewellnet.js" },
+          {
+            src: [
+              "_src/js/lib/jquery-3.3.1.min.js",
+              "_src/js/lib/jquery-scrollspy.js",
+              "_src/js/lib/hammer.js"
+            ],
+            dest: "dist/prod/js/lib.js"
+          },
+          {
+            src: ["_src/js/base.js", "_src/js/splash.js"],
+            dest: "dist/prod/splash/js/dj-splash.js"
+          }
+        ]
       },
       dev: {
         options: {
           output: {
             comments: true,
-            beautify: true,
+            beautify: true
           }
         },
-        files: {
-          'dist/dev/js/jewellnet.js': ['_src/js/base.js'],
-          'dist/dev/js/jquery-scrollspy.js': ['_src/js/jquery-scrollspy.js'],
-          'dist/dev/splash/js/dj-splash.js': ['_src/js/base.js', '_src/js/splash.js'],
-        }
+        files: [
+          { src: "_src/js/base.js", dest: "dist/dev/js/jewellnet.js" },
+          {
+            src: [
+              "_src/js/lib/jquery-3.3.1.min.js",
+              "_src/js/lib/jquery-scrollspy.js",
+              "_src/js/lib/hammer.js"
+            ],
+            dest: "dist/dev/js/lib.js"
+          },
+          {
+            src: ["_src/js/base.js", "_src/js/splash.js"],
+            dest: "dist/dev/splash/js/dj-splash.js"
+          }
+        ]
       }
     },
     jshint: {
@@ -60,95 +90,136 @@ module.exports = function (grunt) {
         eqnull: true,
         browser: true,
         globals: {},
-        reporterOutput: ''
+        reporterOutput: ""
       },
       gruntfile: {
-        src: 'Gruntfile.js'
+        src: "Gruntfile.js"
       },
       lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
+        src: ["lib/**/*.js", "test/**/*.js"]
       }
     },
     qunit: {
-      files: ['test/**/*.html']
+      files: ["test/**/*.html"]
     },
     watch: {
       gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
+        files: "<%= jshint.gruntfile.src %>",
+        tasks: ["jshint:gruntfile"]
       },
       lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
+        files: "<%= jshint.lib_test.src %>",
+        tasks: ["jshint:lib_test", "qunit"]
       },
       default: {
-        files: ['_src/**/*.less', '_src/**/*.js', '_src/**/*.html'],
-        tasks: 'dev'
+        files: ["_src/**/*.less", "_src/**/*.js", "_src/**/*.html"],
+        tasks: "dev"
       }
     },
     less: {
       options: {
-        banner: '<%= banner %>',
-        sourceMap: true,
+        banner: "<%= banner %>",
+        sourceMap: true
       },
       prod: {
         options: {
-          compress: true,
+          compress: true
         },
         files: {
-          'dist/prod/css/jewellnet.min.css': ['_src/less/import-all.less'],
-          'dist/prod/splash/css/dj-splash.min.css': ['_src/less/splash.less'],
+          "dist/prod/css/jewellnet.min.css": ["_src/less/import-all.less"],
+          "dist/prod/splash/css/dj-splash.min.css": ["_src/less/splash.less"]
         }
       },
       dev: {
         options: {
-          compress: false,
+          compress: false
         },
         files: {
-          'dist/dev/css/jewellnet.css': ['_src/less/import-all.less'],
-          'dist/dev/splash/css/dj-splash.css': ['_src/less/splash.less'],
+          "dist/dev/css/jewellnet.css": ["_src/less/import-all.less"],
+          "dist/dev/splash/css/dj-splash.css": ["_src/less/splash.less"]
         }
       }
     },
     copy: {
       dev: {
         files: [
-          { expand: true, cwd: '_src', src: ['img/social/**', 'img/portfolio/**'], dest: 'dist/dev/' },
-          { expand: true, flatten: true, cwd: '_src', src: ['html/*'], dest: 'dist/dev/' },
-          { expand: true, cwd: '_src', src: ['css/*'], dest: 'dist/dev/' }
+          {
+            expand: true,
+            cwd: "_src",
+            src: ["img/social/**", "img/portfolio/**"],
+            dest: "dist/dev/"
+          },
+          {
+            expand: true,
+            flatten: true,
+            cwd: "_src",
+            src: ["html/*"],
+            dest: "dist/dev/"
+          },
+          { expand: true, cwd: "_src", src: ["css/*"], dest: "dist/dev/" }
         ]
       },
       prod: {
         files: [
-          { expand: true, cwd: '_src', src: ['img/social/**', 'img/portfolio/**'], dest: 'dist/prod/' },
-          { expand: true, flatten: true, cwd: '_src', src: ['html/*'], dest: 'dist/prod/' },
-          { expand: true, cwd: '_src', src: ['css/*'], dest: 'dist/prod/' }
+          {
+            expand: true,
+            cwd: "_src",
+            src: ["img/social/**", "img/portfolio/**"],
+            dest: "dist/prod/"
+          },
+          {
+            expand: true,
+            flatten: true,
+            cwd: "_src",
+            src: ["html/*"],
+            dest: "dist/prod/"
+          },
+          { expand: true, cwd: "_src", src: ["css/*"], dest: "dist/prod/" }
         ]
       },
       complete: {
         files: [
-          { expand: true, cwd: '_src', src: ['img/*', 'fonts/**'], dest: 'dist/prod/' },
-          { expand: true, cwd: '_src', src: ['img/*', 'fonts/**'], dest: 'dist/dev/' },
-          { expand: true, cwd: '_src', src: ['js/modernizr.js'], dest: 'dist/prod/' },
-          { expand: true, cwd: '_src', src: ['js/modernizr.js'], dest: 'dist/dev/' },
+          {
+            expand: true,
+            cwd: "_src",
+            src: ["img/*", "fonts/**"],
+            dest: "dist/prod/"
+          },
+          {
+            expand: true,
+            cwd: "_src",
+            src: ["img/*", "fonts/**"],
+            dest: "dist/dev/"
+          },
+          {
+            expand: true,
+            cwd: "_src",
+            src: ["js/modernizr.js"],
+            dest: "dist/prod/"
+          },
+          {
+            expand: true,
+            cwd: "_src",
+            src: ["js/modernizr.js"],
+            dest: "dist/dev/"
+          }
         ]
       }
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
   // Default task.
-  grunt.registerTask('default', ['less:dev', 'uglify:dev']);
+  grunt.registerTask("default", ["less:dev", "uglify:dev"]);
 
-  grunt.registerTask('dev', ['less:dev', 'uglify:dev', 'copy:dev']);
-  grunt.registerTask('prod', ['less:prod', 'uglify:prod', 'copy:prod']);
+  grunt.registerTask("dev", ["less:dev", "uglify:dev", "copy:dev"]);
+  grunt.registerTask("prod", ["less:prod", "uglify:prod", "copy:prod"]);
 
-  grunt.registerTask('complete', ['dev', 'prod', 'copy:complete']);
-
+  grunt.registerTask("complete", ["dev", "prod", "copy:complete"]);
 };
--- _src/js/base.js
+++ _src/js/base.js
@@ -12,12 +12,92 @@ var pageInfo = {
       fwd: ".project__carousel-controls--next"
     },
     position: ".project__carousel-controls--location .pos",
-    total: ".project__carousel-controls--location .total"
+    total: ".project__carousel-controls--location .total",
+    imgs: ".project__carousel--image-wrapper"
   }
 };
 
+// declarations
 var $burgerButton = $(pageInfo.burger.button);
 var $burgerWrapper = $(pageInfo.burger.wrapper);
+
+// object constructors
+function Slideshow(imgs) {
+  this.imgs = imgs; // array of images
+  this.pos = 1; // slideshow position
+  this.total = this.imgs.length; // total number of images
+  this.$back = $(pageInfo.carousel.controls.back);
+  this.$fwd = $(pageInfo.carousel.controls.fwd);
+  this.$positn = $(pageInfo.carousel.position);
+  this.$tot = $(pageInfo.carousel.total);
+
+  console.log("total images: ", this.total);
+
+  this.updateControls = function() {
+    this.$positn.html(this.pos);
+
+    this.pos == this.total
+      ? this.$fwd.addClass("disabled")
+      : this.$fwd.removeClass("disabled");
+
+    this.pos == 1
+      ? this.$back.addClass("disabled")
+      : this.$back.removeClass("disabled");
+
+    this.updateImages();
+
+    return;
+  };
+
+  this.updateImages = function() {
+    $(this.imgs).removeClass("atBat onFirst onDeck inTheHole onSecond");
+
+    $(this.imgs[this.returnPosition() - 1]).addClass("atBat");
+    $(this.imgs[this.returnPosition()]).addClass("onDeck");
+    $(this.imgs[this.returnPosition() + 1]).addClass("inTheHole");
+    $(this.imgs[this.returnPosition() - 2]).addClass("onFirst");
+    $(this.imgs[this.returnPosition() - 3]).addClass("onSecond");
+
+    if (!$(pageInfo.carousel.wrapper).hasClass("activated")) {
+      $(pageInfo.carousel.wrapper).addClass("activated");
+    }
+
+    return;
+  };
+
+  this.returnPosition = function() {
+    return this.pos;
+  };
+
+  this.nextImage = function() {
+    if (this.pos < this.total) {
+      ++this.pos;
+      console.log("➡️");
+    } else {
+      return;
+    }
+
+    return this.updateControls();
+  };
+
+  this.prevImage = function() {
+    if (this.pos !== 1) {
+      --this.pos;
+      console.log("⬅️");
+    } else {
+      return;
+    }
+
+    return this.updateControls();
+  };
+
+  this.init = function() {
+    this.updateControls();
+    this.$tot.html(this.total);
+  };
+
+  this.init();
+}
 
 function toggleNav() {
   console.log("🍔");
@@ -25,3 +105,24 @@ function toggleNav() {
 }
 
 $burgerButton.click(toggleNav);
+
+var show = new Slideshow($(pageInfo.carousel.imgs));
+
+$(pageInfo.carousel.controls.fwd).click(function() {
+  show.nextImage();
+});
+
+$(pageInfo.carousel.controls.back).click(function() {
+  show.prevImage();
+});
+
+// Enable swiping
+var stage = document.querySelector(pageInfo.carousel.wrapper);
+var swipd = new Hammer(stage);
+swipd
+  .on("swipeleft", function(ev) {
+    show.nextImage();
+  })
+  .on("swiperight", function(ev) {
+    show.prevImage();
+  });
