86cf7add425d6ba76702925654f38df389c5aee3
David Jewell <david@david-jewell.net>
2018-10-29 12:29:41 -0400

    Un-burgers detail pages on larger breakpoints. Closes #17

--- _src/vue/_src/components/masthead/siteMenu.vue
+++ _src/vue/_src/components/masthead/siteMenu.vue
@@ -7,7 +7,7 @@
       <ul class="masthead__menu-list">
         <li v-for="item in menuItems" :key="item.key" :class="['masthead__menu-list-' + item.key, {disabled: item.isDisabled}]">
           <router-link v-if="item.type == 'router'" :to="{name: item.key}">{{item.text}}</router-link>
-          <a v-else :href="item.url" >{{item.text}}</a>
+          <a v-else :href="item.url" target="_blank">{{item.text}}</a>
         </li>
       </ul>
     </div>
@@ -25,6 +25,8 @@ export default {
       burgerOpen: false,
       burger: {
         wrapper: ".masthead__menu",
+        menuWrapper: ".masthead__menu-wrapper",
+        menuLength: 0,
         toggled: "open"
       }
     };
@@ -32,17 +34,23 @@ export default {
   components: {
     "burger-icon": burgerSVG
   },
+  updated() {
+    this.burger.menuLength = this.menuItems.length;
+  },
   methods: {
     toggleNav: function() {
       var burgerWrapper = document.querySelector(this.burger.wrapper);
+      var mWrapper = document.querySelector(this.burger.menuWrapper);
 
       if (this.burgerOpen == false) {
         console.log("🍔");
         burgerWrapper.classList.add(this.burger.toggled);
+        mWrapper.style.height = 40 * this.burger.menuLength + "px";
         this.burgerOpen = true;
       } else {
         console.log("🚫🍔");
         burgerWrapper.classList.remove(this.burger.toggled);
+        mWrapper.style.height = 0;
         this.burgerOpen = false;
       }
     }
